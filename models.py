8f3646f28dfd9ed1ded132eb0db8ef1e76a1f238
Adamtheresponsible <122668832+Adamtheresponsible@users.noreply.github.com>
2023-08-21 23:23:04 -0700

    Update models.py
    added suit and card classes

--- models.py
+++ models.py
@@ -1,3 +1,19 @@
 from emun import Enum
 import random
 import pygame
+
+class Suits(Enum):
+  CLUB = 0
+  SPADE = 1
+  HEART = 2
+  DIAMOND = 3
+
+class Card:
+  suit = None
+  value = None
+  image = None
+
+  def __init__(self, suit, value):
+    self.suit = suit
+    self.value = value
+    self.image = pygame.image.load('image/' + self.suit.name + '-' str(self.value) + '.svg')
