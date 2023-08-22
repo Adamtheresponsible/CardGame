6264931d9409161f66660d33099eebde2ea5c8a9
Adamtheresponsible <122668832+Adamtheresponsible@users.noreply.github.com>
2023-08-22 11:05:30 -0700

    Update models.py
    added Player class

--- models.py
+++ models.py
@@ -62,3 +62,21 @@ class Pile:
       return (self.cards[-1].value == self.cards[-2].value)
     else:
       return False
+
+class Player:
+  hand = None
+  flipKey = None
+  snapKey = None
+  name = None
+
+  def __init__(self, name, flipKey, snapKey):
+    self.hand = []
+    self.flipKey = flipKey
+    self.snapKey = snapKey
+    self.name = name
+
+  def draw(self, deck):
+    self.hand.append(deck.deal())
+
+  def play(self):
+    return self.hand.pop(0)
