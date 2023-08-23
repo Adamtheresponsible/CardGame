4869053aa3a1e6f2c792ed17f52a7c0ee942cba2
Adamtheresponsible <122668832+Adamtheresponsible@users.noreply.github.com>
2023-08-23 01:19:19 -0700

    Update models.py
    added missing +

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class Card:
   def __init__(self, suit, value):
     self.suit = suit
     self.value = value
-    self.image = pygame.image.load('image/' + self.suit.name + '-' str(self.value) + '.svg')
+    self.image = pygame.image.load('image/' + self.suit.name + '-' + str(self.value) + '.svg')
 
 class Deck:
   cards = None
