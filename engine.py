cc204d64b37c8f67c33ef7102a6065c2193f953c
Adamtheresponsible <122668832+Adamtheresponsible@users.noreply.github.com>
2023-08-23 02:30:17 -0700

    Update engine.py
    Added SnapEngine class

--- engine.py
+++ engine.py
@@ -7,3 +7,22 @@ class GameState(Enum):
   SNAPPING = 1
   ENDED = 2
 
+class SnapEngine:
+  deck = None
+  player1 = None
+  player2 = None
+  pile = None
+  state = None
+  currentPlayer = None
+  recult = None
+
+  def __init__(self):
+    self.deck = Deck()
+    self.deck.shuffle()
+    self.player1 = Player("Player 1", pygame.K_q, pygame.K_w)
+    self.player2 = Player("Player 2", pygame.K_o, pygame.K_p)
+    self.pile = Pile()
+    self.deal()
+    self.currentPlayer = self.player1
+    self.state = GameState.PLAYING
+
