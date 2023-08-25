f1416eeb927daf99f7440fa8d9c6053351682f00
Adamtheresponsible <122668832+Adamtheresponsible@users.noreply.github.com>
2023-08-24 21:05:33 -0700

    Update main.py
    added game loop

--- main.py
+++ main.py
@@ -8,3 +8,21 @@ window = pygame.display.set_mode(bounds)
 pygame.display.set_caption("SnaPy")
 
 gameEngine = SnapEngine()
+
+run = True
+while run:
+  key = None;
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+    if event.type == pygame.KEYDOWN:
+      key = event.key
+
+  gameEngine.play(key)
+  renderGame(window)
+  pygame.display.update()
+
+  if gameEngine.state == GameState.SNAPPING:
+    pygame.time.display(3000)
+    gameEngine.state = GameState.PLAYING
+
