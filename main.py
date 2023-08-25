085fbb67c621c1d5d2dbbd8583826c7124c02aa8
Adamtheresponsible <122668832+Adamtheresponsible@users.noreply.github.com>
2023-08-24 20:59:44 -0700

    Update main.py
    added pygame import, initialized pygame

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import pygame
 from models import *
 from engine import *
+
+pygame.init()
+bounds = (1024, 768)
+window = pygame.display.set_mode(bounds)
+pygame.display.set_caption("SnaPy")
 
 gameEngine = SnapEngine()
